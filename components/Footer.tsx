b720428adc17d50b8ceaf64849f913ed87285450
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-02-19 20:08:25 +0400

    navbar-footer

---
+++ components/Footer.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { footerLinks } from "@/constants";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="flex max-w-[1440px] mx-auto flex-col text-black-100 mt-5 border-t-2 border-gray-100">
+      <div className="flex  max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            Rentify 2023 <br />
+            2024 Rentify. All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
