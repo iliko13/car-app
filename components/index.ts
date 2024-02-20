a44b8e4d1edae11b10e8a87dacb8b9e261302016
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-02-20 20:46:54 +0400

    third commit

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,20 @@ import Hero from "@/components/Hero";
 import CustomButton from "@/components/CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Navbar, Hero, CustomButton, Footer };
+export {
+  Navbar,
+  Hero,
+  SearchBar,
+  CustomButton,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  Footer,
+};
--- types/index.ts
+++ types/index.ts
@@ -5,4 +5,27 @@ export type CustomButtonProps = {
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+};
+
+export type SearchManufacturerProps = {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+};
+
+export type CarProps = {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 };
