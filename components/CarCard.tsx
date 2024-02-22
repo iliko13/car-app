41b61e964837ce8493cc8a59558a6311716b3068
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-02-22 17:16:06 +0400

    third commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import Image from "next/image";
 import { CarDetails, CustomButton } from ".";
 import { useState } from "react";
@@ -31,7 +31,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
